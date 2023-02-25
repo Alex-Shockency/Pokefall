f7636a242d9f541fc986c7402a3d49e1f68070fa
alex_shockency <alexshockency@gmail.com>
2023-02-24 23:53:26 -0700

    Update add abilities to use csv reader for more consistent results

--- poke-fall-api/Migrations/20230225022636_AddingAbilities.cs
+++ poke-fall-api/Migrations/20230225022636_AddingAbilities.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+using CsvHelper;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+using poke_fall_api.Models;
 
 #nullable disable
 
@@ -13,40 +17,79 @@ namespace pokefallapi.Migrations
         {
             migrationBuilder.CreateTable(
                 name: "Abilities",
-                columns: table => new
-                {
-                    Id = table.Column<int>(type: "integer", nullable: false)
-                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
-                    Name = table.Column<string>(type: "text", nullable: false),
-                    Description = table.Column<string>(type: "text", nullable: false)
-                },
+                columns: table =>
+                    new
+                    {
+                        Id = table
+                            .Column<int>(type: "integer", nullable: false)
+                            .Annotation(
+                                "Npgsql:ValueGenerationStrategy",
+                                NpgsqlValueGenerationStrategy.IdentityByDefaultColumn
+                            ),
+                        Name = table.Column<string>(type: "text", nullable: false),
+                        Description = table.Column<string>(type: "text", nullable: false)
+                    },
                 constraints: table =>
                 {
                     table.PrimaryKey("PK_Abilities", x => x.Id);
-                });
+                }
+            );
 
-            string[] columns = new string[]{"Id", "Name", "Description"};
-
-            foreach (string line in File.ReadLines(@"/Users/akmindt/Projects/pokeapi/data/v2/csv/abilities.csv"))
+            string[] columns = new string[] { "Id", "Name", "Description" };
+            using (var abilReader = new StreamReader(@"./data/abilities.csv"))
+            using (var abilCsv = new CsvReader(abilReader, CultureInfo.InvariantCulture))
+            {
+                var abilities = abilCsv.GetRecords<PokeApiAbility>().ToArray();
+                using (
+                    var flavorReader = new StreamReader(@"./data/flavortext.csv")
+                )
+                using (var flavorCsv = new CsvReader(flavorReader, CultureInfo.InvariantCulture))
                 {
-                    string[] values = line.Split(",");
-                        foreach (string descLine in File.ReadLines(@"/Users/akmindt/Projects/pokeapi/data/v2/csv/ability_flavor_text.csv"))
+                    var flavorTexts = flavorCsv.GetRecords<PokeApiFlavorText>().ToArray();
+                    foreach (PokeApiAbility ability in abilities)
+                    {
+                        foreach (PokeApiFlavorText text in flavorTexts)
                         {
-                            string[] descValues = descLine.Split(",");
-                            if (descValues[2].Equals("9") && Int32.Parse(descValues[1]) >= 10 && values[0].Equals(descValues[0]))
+                            if (
+                                ability.id == text.ability_id
+                                && text.version_group_id == 20
+                                && text.language_id == 9
+                            )
                             {
-                                string[] dbValues = new string[]{values[0], values[1], descValues[3]};
+                                string[] dbValues = new string[]
+                                {
+                                    ability.id.ToString(),
+                                    ability.identifier,
+                                    text.flavor_text
+                                };
                                 migrationBuilder.InsertData("Abilities", columns, dbValues);
                             }
                         }
                     }
+                }
+            }
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropTable(
-                name: "Abilities");
+            migrationBuilder.DropTable(name: "Abilities");
+        }
+
+        private class PokeApiAbility
+        {
+            public int id { get; set; }
+            public string identifier { get; set; }
+            public int generation_id { get; set; }
+            public bool is_main_series { get; set; }
+        }
+
+        private class PokeApiFlavorText
+        {
+            public int ability_id { get; set; }
+            public int version_group_id { get; set; }
+            public int language_id { get; set; }
+            public string flavor_text { get; set; }
         }
     }
 }
