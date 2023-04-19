dda37a643a38538ff061089c5c35de7fb9a349ad
alex_shockency <alexshockency@gmail.com>
2023-04-19 06:34:41 -0600

    Add radar chart and toggling feature

--- poke-fall/src/app/Components/pokemon-info/pokemon-info.component.ts
+++ poke-fall/src/app/Components/pokemon-info/pokemon-info.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Pokemon } from 'src/app/Entities/pokemon';
 import { PokemonService } from 'src/app/Services/pokemon-service';
@@ -13,31 +13,103 @@ import { MatAccordion } from '@angular/material/expansion';
 export class PokemonInfoComponent {
   @ViewChild(MatAccordion)
   accordion!: MatAccordion;
-  
+
   pokemonId = 0;
   pokemon!: Pokemon;
-  chart: any;
+  barChart: any;
+  radarChart: any;
+  showBarChart = true;
+  showRadarChart = false;
+  audioUrl = '';
+
+  pokeCry!: HTMLMediaElement;
 
   constructor(
     private pokemonService: PokemonService,
-    private route: ActivatedRoute,
+    private route: ActivatedRoute
   ) {}
 
   ngOnInit() {
-    window.scrollTo(0,0)
+    window.scrollTo(0, 0);
     this.route.queryParams.subscribe((params) => {
       this.pokemonId = params['id'];
       this.pokemonService
         .getPokemonById(this.pokemonId)
         .subscribe((pokemon) => {
           this.pokemon = pokemon;
-          this.createChart();
+          this.pokeCry = document.getElementById(
+            'audioElement'
+          ) as HTMLMediaElement;
+          this.pokeCry.src =
+            'https://play.pokemonshowdown.com/audio/cries/' +
+            this.formatName(pokemon.name) +
+            '.ogg';
+          this.createBarChart();
+          this.createRadarChart();
         });
     });
   }
 
-  createChart() {
-    this.chart = new Chart('myChart', {
+  createRadarChart() {
+    this.radarChart = new Chart('radarChart', {
+      type: 'radar', //this denotes tha type of chart
+
+      data: {
+        // values on X-Axis
+        labels: [
+          'HP',
+          'Attack',
+          'Defense',
+          'Sp. Attack',
+          'Sp. Defense',
+          'Speed',
+        ],
+        datasets: [
+          {
+            data: [
+              this.pokemon.hp,
+              this.pokemon.attack,
+              this.pokemon.defense,
+              this.pokemon.specAttack,
+              this.pokemon.specDefense,
+              this.pokemon.speed,
+            ],
+            backgroundColor: ['rgba(255, 99, 132, 0.2)'],
+            borderColor: 'rgb(255, 99, 132)',
+            pointBackgroundColor: [
+              'rgba(255, 99, 132 )',
+              'rgba(255, 159, 64 )',
+              'rgba(255, 205, 86)',
+              'rgba(75, 192, 192)',
+              'rgba(54, 162, 235)',
+              'rgba(153, 102, 255)',
+              'rgba(201, 203, 207)',
+            ],
+            pointBorderColor: '#fff',
+            pointHoverBackgroundColor: '#fff',
+            pointHoverBorderColor: 'rgb(255, 99, 132)',
+          },
+        ],
+      },
+      options: {
+        plugins: {
+          legend: {
+            display: false,
+          },
+        },
+        scales: {
+          r: {
+            min: 0,
+            beginAtZero: true,
+          },
+        },
+        aspectRatio: 2.5,
+      },
+    });
+  }
+
+  createBarChart() {
+    this.barChart = new Chart('barChart', {
       type: 'bar', //this denotes tha type of chart
 
       data: {
@@ -79,15 +151,31 @@ export class PokemonInfoComponent {
           },
         },
         scales: {
-          x:{
+          x: {
             max: 255,
-            beginAtZero: true
-          }
+            beginAtZero: true,
+          },
         },
         indexAxis: 'y',
         aspectRatio: 2.5,
       },
     });
+  }
+  protected playCry(id: number) {
+    this.pokeCry.play();
+  }
+
+  toggleOnBar() {
+    this.showBarChart = true;
+    this.showRadarChart = false;
+  }
+  toggleOnRadar(){
+    this.showRadarChart =true;
+    this.showBarChart = false;
+  }
+
+  formatName(name: string) {
+    return name.toLowerCase();
   }
 
   padId(id: number): string {
